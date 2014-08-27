f8c7ed7fd9d0d37355296e4a671907dbf8a712df
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-08-27 23:03:57 +0100

    changed for 0.9.0

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.on_use(function (api) {
 	api.use('jquery', 'client');
-	api.use('moment', 'client');	
+	api.use('mrt:moment', 'client');	
 
 	var path = Npm.require('path');
 	api.add_files('client/pikaday.css', 'client');
