294fa0f9ccddfcf5f2d246ffdd522ce3ec54c3c3
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-08-27 23:09:29 +0100

    changed for 0.9.0

--- package.js
+++ package.js
@@ -6,8 +6,9 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+	api.versionsFrom(METEOR-CORE@0.9.0);
 	api.use('jquery', 'client');
-	api.use('mrt:moment', 'client');	
+	api.use('mrt:moment@2.8.1', 'client');	
 
 	var path = Npm.require('path');
 	api.add_files('client/pikaday.css', 'client');
